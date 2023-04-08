c386ae9bb6151fafcd3b87d83c148faebb03db0b
Nicolas Germeau <ngermeau@icloud.com>
2023-04-08 14:26:06 +0200

    avoid failing if error when fetching the detail of a doctor

--- src/main/java/be/nicolasgermeau/App.java
+++ src/main/java/be/nicolasgermeau/App.java
@@ -9,10 +9,6 @@ import org.jsoup.select.Elements;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 
 /**
@@ -21,36 +17,41 @@ import java.nio.file.Paths;
 public class App {
     private final static Integer STARTING_PAGE = 1;
     private final static Integer ENDING_PAGE = 418;
-    private final static String filename = "doctors";
-    private final static File file = new File(filename);
-    private static final FileWriter outputfile = new FileWriter(file);
-    CSVWriter writer = new CSVWriter(outputfile);
+    private final static String filename = "doctors.csv";
+    private CSVWriter writer;
 
-    public static void write(String[] doctorData) throws Exception {
-//        Paths.ge
-        String[] ar = {"hello","world"};
-        writer.writeNext(ar);
-        writer.close();
+    public void write(String[] doctorData) throws Exception {
+        this.writer.writeNext(doctorData);
+        this.writer.flush();
     }
 
-    public App() {
+    public App() throws IOException {
+        File file = new File(filename);
+        FileWriter outputFile = new FileWriter(file);
+        writer = new CSVWriter(outputFile);
     }
-
-    public static void main(String[] args) throws Exception {
-        App.write();
+    
+    private void scrap() throws Exception {
         for (int currentPage = STARTING_PAGE; currentPage <= ENDING_PAGE; currentPage++) {
             Document doctorsListPage = Jsoup.connect("https://www.medecinbelgique.com/medecin/rechercher-un-medecin.htm/page:" + currentPage + "?data[Doctor][specialties]=&data[Doctor][city_id]=Bruxelles&data[Doctor][nom]=&data[Doctor][prenom]=&data[Doctor][codepostal]=").get();
             Elements doctorsLink = doctorsListPage.select("div.strip_list > a");
             for (Element doctorLink : doctorsLink) {
                 String link = doctorLink.attr("href");
-                Document doctorDetailPage = Jsoup.connect("https://www.medecinbelgique.com" + link).get();
-                String specialities = doctorDetailPage.select("div.profile small").first().text();
-                String name = doctorDetailPage.select("div.profile h1").first().text();
-                String address = doctorDetailPage.select("ul.contacts li").first().text().replace("Adresse principale", "");
-                String telephone = doctorDetailPage.select("ul.contacts li").get(1).text().replace("Téléphone : ", "");
-                String[] doctorData = {String.valueOf(currentPage),specialities,name,address,telephone};
-                write(doctorData);
+                try {
+                    Document doctorDetailPage = Jsoup.connect("https://www.medecinbelgique.com" + link).get();
+                    String specialities = doctorDetailPage.select("div.profile small").first().text();
+                    String name = doctorDetailPage.select("div.profile h1").first().text();
+                    String address = doctorDetailPage.select("ul.contacts li").first().text().replace("Adresse principale", "").replace("Voir sur la carte", "");
+                    String telephone = doctorDetailPage.select("ul.contacts li").get(1).text().replace("Téléphone : ", "");
+                    String[] doctorData = {String.valueOf(currentPage), specialities, name, address, telephone};
+                    write(doctorData);
+                }catch (Exception exception){
+                    System.out.println("error while fetching url" + link);
+                }
             }
         }
+    }
+    public static void main(String[] args) throws Exception {
+        new App().scrap();
     }
 }
